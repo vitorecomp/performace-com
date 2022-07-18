03645374b109b6ffb8ab658d58a21b3935c487c2
vitorecomp <vitor.ecomp@gmail.com>
2022-07-18 17:10:52 -0300

    feat: build the spring tomcat app with sleep

--- spring/src/main/java/nobuffer/dev/service/rest/SleepResources.java
+++ spring/src/main/java/nobuffer/dev/service/rest/SleepResources.java
@@ -5,7 +5,6 @@ import nobuffer.dev.service.domain.Sleep;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
