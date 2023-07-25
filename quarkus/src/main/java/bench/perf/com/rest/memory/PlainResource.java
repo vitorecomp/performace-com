22900f42a614f5e9ff20233e8a7a2f92a708e323
vitorecomp <vitor.ecomp@gmail.com>
2023-07-25 17:10:44 -0300

    feat: create the plain service, and implment the string endpoints

--- quarkus/src/main/java/bench/perf/com/rest/memory/PlainResource.java
+++ quarkus/src/main/java/bench/perf/com/rest/memory/PlainResource.java
@@ -5,64 +5,83 @@ import java.util.List;
 
 import bench.perf.com.domain.memory.ComplexStructure;
 import bench.perf.com.domain.memory.SimpleStructure;
+import bench.perf.com.service.memory.PlainService;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.DefaultValue;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.QueryParam;
 
 @Path("/plain-benchmarks")
 public class PlainResource {
 
+    PlainService plainService;
+
+    @Inject
+    public PlainResource(PlainService plainService) {
+        this.plainService = plainService;
+    }
+
+
     @GET
     @Path("/plain-texts")
-    public String getPlain() {
-        return "Hello from my benchmark application!";
+    public List<String> getPlain(@QueryParam("messageSize") @DefaultValue("1000") Integer messageSize, @QueryParam("numberOfMessages") @DefaultValue("1000") Integer numberOfMessages) {
+        return plainService.plainStringList(messageSize, numberOfMessages);
+    }
+
+
+    @GET
+    @Path("/plain-texts/{id}")
+    public String getPlain(@QueryParam("messageSize") @DefaultValue("1000") Integer messageSize, @PathParam Integer id) {
+        return plainService.plainString(id, messageSize);
     }
 
     @GET
     @Path("/simple-json")
     public List<SimpleStructure> getSimpleJsonList() {
-        return Collections.emptyList();
-
+        
     }
 
     @GET
     @Path("/simple-json/{id}")
     public SimpleStructure getSimpleJson() {
-        return new SimpleStructure();
+        return plainService.simpleStructure();
     }
 
     @GET
     @Path("/complex-json")
     public List<ComplexStructure> getComplexJsonList() {
-        return Collections.emptyList();
+        return plainService.complexStructureList();
     }
 
     @GET
     @Path("/complex-json/{id}")
     public ComplexStructure getComplexJson() {
-        return new ComplexStructure();
+        return plainService.complexStructure();
     }
 
     @GET
     @Path("/small-files")
-    public String getSmallFiles() {
-        return "";
+    public List<String> getSmallFiles() {
+        return plainService.smallFilesRead();
     }
 
     @GET
     @Path("/small-files/{id}")
     public String getSmallFile() {
-        return "";
+        return plainService.smallFileRead();
     }
 
     @GET
     @Path("/large-files")
-    public String getLargeFiles() {
-        return "";
+    public List<String> getLargeFiles() {
+        return plainService.largeFilesRead();
     }
 
     @GET
     @Path("/large-files/{id}")
     public String getLargeFile() {
-        return "";
+        return plainService.largeFileRead();
     }
 }
--- quarkus/src/main/java/bench/perf/com/service/memory/PlainService.java
+++ quarkus/src/main/java/bench/perf/com/service/memory/PlainService.java
@@ -1,5 +1,63 @@
 package bench.perf.com.service.memory;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import bench.perf.com.domain.memory.ComplexStructure;
+import bench.perf.com.domain.memory.SimpleStructure;
+import bench.perf.com.utility.MessageUtils;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
 public class PlainService {
-    
+
+    // plain text string services
+    public String plainString(Integer id, Integer messageSize) {
+        return id.toString() + "-" + MessageUtils.generateMessage(messageSize);
+    }
+
+    public List<String> plainStringList(Integer messageSize, Integer numberOfMessages) {
+        List<String> strings = new ArrayList<>(numberOfMessages);
+        for (Integer i = 0; i < numberOfMessages; i++) {
+            strings.set(i, MessageUtils.generateMessage(messageSize));
+        }
+        return strings;
+    }
+
+    // plain text json services
+    public SimpleStructure simpleStructure() {
+        return null;
+    }
+
+    public SimpleStructure simpleStructureList() {
+        return null;
+    }
+
+    // complex json services
+    public ComplexStructure complexStructure() {
+        return null;
+    }
+
+    public List<ComplexStructure> complexStructureList() {
+        return null;
+    }
+
+    // small file services
+    public List<String> smallFilesRead() {
+        return null;
+    }
+
+    public String smallFileRead() {
+        return null;
+    }
+
+    // large file services
+    public List<String> largeFilesRead() {
+        return null;
+    }
+
+    public String largeFileRead() {
+        return null;
+    }
+
 }
