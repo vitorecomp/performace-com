28586291a83aa4328432adeb933f9c5dc34f3695
vitorecomp <vitor.ecomp@gmail.com>
2023-07-31 13:36:14 -0300

    feat: add the 404 capability on the system files

--- quarkus/src/main/java/bench/perf/com/rest/filesystem/FileSystemResource.java
+++ quarkus/src/main/java/bench/perf/com/rest/filesystem/FileSystemResource.java
@@ -6,10 +6,14 @@ import bench.perf.com.service.filesystem.FileSystemService;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.DefaultValue;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.QueryParam;
 
 @ApplicationScoped
 @Path("/filesystem")
@@ -24,62 +28,84 @@ public class FileSystemResource {
 
     @GET
     @Path("/small-files")
-    public List<String> getSmallFiles() {
-        return fileSystemService.smallFilesRead();
+    public List<String> getSmallFiles(@QueryParam("limit") @DefaultValue("100") Integer limit,
+            @QueryParam("offset") @DefaultValue("0") Integer offset) {
+        return fileSystemService.readSmallFiles(limit, offset);
     }
 
     @POST
     @Path("/small-files")
-    public List<String> postSmallFiles() {
-        return fileSystemService.smallFilesRead();
+    public String postSmallFiles(String fileContent) {
+        return fileSystemService.saveSmallFile(fileContent);
     }
 
     @PUT
-    @Path("/small-files")
-    public List<String> putSmallFiles() {
-        return fileSystemService.smallFilesRead();
+    @Path("/small-files/{id}")
+    public String putSmallFiles(@PathParam("id") String id, String fileContent) {
+        String responseFile = fileSystemService.updateSmallFile(id, fileContent);
+        if (responseFile == null) {
+            throw new NotFoundException();
+        }
+        return responseFile;
     }
 
     @GET
     @Path("/small-files/{id}")
-    public String getSmallFile() {
-        return fileSystemService.smallFileRead();
+    public String getSmallFile(@PathParam("id") String id) {
+        String responseFile = fileSystemService.readSmallFile(id);
+        if (responseFile == null) {
+            throw new NotFoundException();
+        }
+        return responseFile;
     }
 
     @DELETE
     @Path("/small-files/{id}")
-    public String deleteSmallFile() {
-        return fileSystemService.smallFileRead();
+    public String deleteSmallFile(@PathParam("id") String id) {
+        return fileSystemService.deleteSmallFile(id);
     }
 
     @GET
     @Path("/large-files")
-    public List<String> getLargeFiles() {
-        return fileSystemService.largeFilesRead();
+    public List<String> getLargeFiles(@QueryParam("limit") @DefaultValue("100") Integer limit,
+            @QueryParam("offset") @DefaultValue("0") Integer offset) {
+        return fileSystemService.readLargeFiles(limit, offset);
     }
 
     @POST
     @Path("/large-files")
-    public List<String> postLargeFiles() {
-        return fileSystemService.smallFilesRead();
+    public String postLargeFiles(String fileContent) {
+        return fileSystemService.saveLargeFile(fileContent);
     }
 
     @PUT
     @Path("/large-files")
-    public List<String> putLargeFiles() {
-        return fileSystemService.smallFilesRead();
+    public String putLargeFiles(@PathParam("id") String id, String fileContent) {
+        String responseFile = fileSystemService.updateLargeFile(id, fileContent);
+        if (responseFile == null) {
+            throw new NotFoundException();
+        }
+        return responseFile;
     }
 
     @GET
     @Path("/large-files/{id}")
-    public String getLargeFile() {
-        return fileSystemService.largeFileRead();
+    public String getLargeFile(@PathParam("id") String id) {
+        String responseFile = fileSystemService.readLargeFile(id);
+        if (responseFile == null) {
+            throw new NotFoundException();
+        }
+        return responseFile;
     }
 
     @DELETE
     @Path("/large-files/{id}")
-    public String deleteLargeFile() {
-        return fileSystemService.largeFileRead();
+    public String deleteLargeFile(@PathParam("id") String id) {
+        String responseFile = fileSystemService.deleteLargeFileRead(id);
+        if (responseFile == null) {
+            throw new NotFoundException();
+        }
+        return responseFile;
     }
 
 }
--- quarkus/src/main/java/bench/perf/com/service/filesystem/FileSystemService.java
+++ quarkus/src/main/java/bench/perf/com/service/filesystem/FileSystemService.java
@@ -6,26 +6,50 @@ import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class FileSystemService {
-        // small file services
-    public List<String> smallFilesRead() {
-        //look if the small dir exists
+    public String saveSmallFile(String fileContent) {
+        return null;
+    }
+
+    public String updateSmallFile(String id, String fileContent) {
+        return null;
+    }
+
+    public List<String> readSmallFiles(Integer limit, Integer offset) {
+                //look if the small dir exists
         //look if the number of files is small that the maximum
         //in case not create a new files and read them
         return null;
     }
 
-    public String smallFileRead() {
-        //look if the id is in the max, if not return error
+    public String readSmallFile(String id) {
+                //look if the id is in the max, if not return error
         //create of read the id file
         return null;
     }
 
-    // large file services
-    public List<String> largeFilesRead() {
+    public String deleteSmallFile(String id) {
         return null;
     }
 
-    public String largeFileRead() {
+    //large file services
+    public List<String> readLargeFiles(Integer limit, Integer offset) {
         return null;
     }
+
+    public String saveLargeFile(String fileContent) {
+        return null;
+    }
+
+    public String deleteLargeFileRead(String id) {
+        return null;
+    }
+
+    public String readLargeFile(String id) {
+        return null;
+    }
+
+    public String updateLargeFile(String id, String fileContent) {
+        return null;
+    }
+
 }
